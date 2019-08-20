59ee188c3f9919715974a393960b4c372ea4865b
Alumno <ulsa@iMacD105.local>
2019-08-20 14:33:53 -0700

    animacion conejita

--- Secuencias/ViewController.swift
+++ Secuencias/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func doTapConejo(_ sender: Any) {
+        if imgSecuencia.isAnimating{
+            imgSecuencia.stopAnimating()
+        }else {
+            imgSecuencia.startAnimating()
+        }
+    }
+    @IBOutlet weak var imgSecuencia: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Arreglo de UIImage con las imagenes de la secuencia
+        var imagenesSecuenciaConejo: [UIImage] = []
+        //aqui se agrega la imagen al arreglo de la seuencia,el simbolo de admiracion se usa cuando dice que el tipo de dato es opcional y nosotros estamos seguros de que es correcto
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo1")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo2")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo3")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo4")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo5")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo6")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo7")!)
+        
+        //se establece la secuenca de imagen
+        imgSecuencia.animationImages = imagenesSecuenciaConejo
+        imgSecuencia.animationDuration = 1
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
